a411ba6942eb74c6d483c4d52d0d958135102bbf
Claudius Mbemba <mbembac@gmail.com>
2018-12-05 23:20:00 -0800

    updated docker build

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo "missing image tag"
-	exit 1
+    echo "missing image tag"
+    exit 1
 fi
 
-docker build -t user1m/neuweb:$1 \
-	-f $CURRENT_PROJECT_PATH/docker/Dockerfile \
-	$CURRENT_PROJECT_PATH/.
+# if [ "$1" == 'prod' ]; then
+FILE="$PROJECT_DIR/docker/Dockerfile"
+# else
+#     FILE="$PROJECT_DIR/docker/Dockerfile.dev"
+# fi
+
+# docker build -t user1m/neuweb:$1 \
+# 	-f $CURRENT_PROJECT_PATH/docker/Dockerfile \
+# 	$CURRENT_PROJECT_PATH/.
+
+docker build \
+-f $FILE \
+-t $IMAGE_TAG \
+$PROJECT_DIR/.
+
